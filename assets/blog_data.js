e61fa4bf0e048a8b80c51de350708d873f85aa8f
sheetmetaldesign1-pixel <sheetmetaldesign1@gmail.com>
2026-01-20 17:48:49 +0530

    add files

---
+++ assets/blog_data.js
@@ -0,0 +1,26 @@
+window.blogPosts = [
+  {
+    "filename": "ultimate-guide-trends-metal-design-2033.html",
+    "url": "posts/ultimate-guide-trends-metal-design-2033.html",
+    "title": "The Ultimate Guide to Trends in Metal Design 2033: Innovations, Engineering, and Sustainability",
+    "description": "Read this article to learn more.",
+    "date": "2026-01-09",
+    "isoDate": "2026-01-09T15:04:55.447Z",
+    "tags": [
+      "Trends"
+    ],
+    "image": "assets/images/default-blog.jpg"
+  },
+  {
+    "filename": "trends-in-metal-design-2035-ultimate-guide.html",
+    "url": "posts/trends-in-metal-design-2035-ultimate-guide.html",
+    "title": "Trends in Metal Design 2035: The Ultimate Guide for Architects, Engineers, and BIM Managers",
+    "description": "Read this article to learn more.",
+    "date": "2026-01-09",
+    "isoDate": "2026-01-09T15:04:55.412Z",
+    "tags": [
+      "Trends"
+    ],
+    "image": "assets/images/default-blog.jpg"
+  }
+];
