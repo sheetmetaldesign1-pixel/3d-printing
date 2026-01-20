e3a938769adeebd117b5b363ceaddd08f7f035a0
sheetmetaldesign1-pixel <sheetmetaldesign1@gmail.com>
2026-01-20 19:13:16 +0530

    Update blog list

--- scripts/generate_blog_data.js
+++ scripts/generate_blog_data.js
@@ -7,5 +7,14 @@ window.blogPosts = [
     "tags": [
       "Trends"
     ]
+  },
+  {
+    "url": "posts/undefined",
+    "image": "assets/images/default-blog.jpg",
+    "date": "2026-01-20",
+    "description": "Read our latest analysis on undefined",
+    "tags": [
+      "Trends"
+    ]
   }
 ];
